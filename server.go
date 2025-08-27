37d3c7f82d93ae8973a18927e4665f1974a3d5e2
aayushxrj <aayush.ranjan0904@gmail.com>
2025-08-27 22:00:48 +0530

    add custom TLS config

--- server.go
+++ server.go
@@ -9,50 +9,79 @@ import (
 	"golang.org/x/net/http2"
 )
 
-func main(){
+func main() {
 
-    port := ":3000"
+	port := ":3000"
 
-    // handling routes 
-    http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Handling incoming orders." )
-    })
+	// handling routes
+	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
+		fmt.Fprintln(w, "Handling incoming orders.")
+	})
 
-    http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Handling users." )
-    })
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
+		fmt.Fprintln(w, "Handling users.")
+	})
 
-    // Load the TLS cert and key 
-    cert := "cert.pem"
-    key := "key.pem"
+	// Load the TLS cert and key
+	cert := "cert.pem"
+	key := "key.pem"
 
-    // Configure TLS 
-    tlsConfig := &tls.Config{
-        MinVersion: tls.VersionTLS12,
-    }
+	// Configure TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
 
-    // Create a custom server 
-    server := &http.Server{
-        Addr: port,
-        Handler: nil, // use default mux
-        TLSConfig: tlsConfig,
-    }
+	// Create a custom server
+	server := &http.Server{
+		Addr:      port,
+		Handler:   nil, // use default mux
+		TLSConfig: tlsConfig,
+	}
 
-    // Enable http2
-    http2.ConfigureServer(server, &http2.Server{})
+	// Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
 
-    fmt.Println("Server Listening on port", port)
+	fmt.Println("Server Listening on port", port)
 
-    err := server.ListenAndServeTLS(cert, key)
-    if err != nil {
-        log.Fatal("Error starting server:", err)
-    }
+	err := server.ListenAndServeTLS(cert, key)
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
 
-    
-    // HTTP 1.1 server without TLS
-    // err := http.ListenAndServe(port, nil)
+	// HTTP 1.1 server without TLS
+	// err := http.ListenAndServe(port, nil)
 
-    // if err != nil {
-    //     log.Fatal("Error starting server:", err)
-    // }
+	// if err != nil {
+	//     log.Fatal("Error starting server:", err)
+	// }
+
+}
+
+func logRequestDetails(r *http.Request) {
+	httpVersion := r.Proto
+	fmt.Println("Received request with HTTP Version:", httpVersion)
+
+	if r.TLS != nil {
+		tlsVersion := getTLSVersionName(r.TLS.Version)
+		fmt.Println("Received request with TLS Version:", tlsVersion)
+	} else {
+		fmt.Println("Received request without TLS")
+	}
+}
+
+func getTLSVersionName(version uint16) string {
+	switch version {
+	case tls.VersionTLS10:
+		return "TLS 1.0"
+	case tls.VersionTLS11:
+		return "TLS 1.1"
+	case tls.VersionTLS12:
+		return "TLS 1.2"
+	case tls.VersionTLS13:
+		return "TLS 1.3"
+	default:
+		return "Unknown TLS Version"
+	}
 }
